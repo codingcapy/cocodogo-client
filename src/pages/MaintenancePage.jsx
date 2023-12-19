59631bda274a16595023d5f96ac25aa2458f3824
Paul K <spkim0921@gmail.com>
2023-12-19 13:23:56 -0800

    5th commit
    maintenance page wording update

--- src/pages/MaintenancePage.jsx
+++ src/pages/MaintenancePage.jsx
@@ -4,7 +4,7 @@ export default function MaintenancePage() {
 
     return (
         <div>
-            <h1 className="py-5 text-2xl text-slate-700 font-medium text-center">We're sorry, CocoDogo is unavailable right now T_T Please check back on December 20, 2023</h1>
+            <h1 className="py-5 text-2xl text-slate-700 font-medium text-center">We're sorry, CocoDogo is under maintenance right now T_T Please visit us back tomorrow at 10:00 AM!</h1>
         </div>
     )
 }
