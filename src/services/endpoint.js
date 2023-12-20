6a3bcadf6d1142b528869a9fb55e02636e51c88a
Paul K <spkim0921@gmail.com>
2023-12-19 16:45:47 -0800

    7th commit
    resume operations using railway server

--- src/services/endpoint.js
+++ src/services/endpoint.js
@@ -6,6 +6,6 @@ version: 1.0
 description: endpoint for CocoDogo
  */
 
-const DOMAIN = "https://cocodogoapi.onrender.com"
+const DOMAIN = "https://cocodogo-server-production.up.railway.app"
 
 export default DOMAIN;
