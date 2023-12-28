054ba405bc29df593b3e2ef336bc9b65fe91d954
Paul K <spkim0921@gmail.com>
2023-12-27 16:05:52 -0800

    10th commit
    implementing post pages

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -115,6 +115,23 @@ export default function PostsPage() {
         )
     }
 
+    async function getPageData(n) {
+        const res = await axios.get(`${DOMAIN}/api/posts/page/${n}`)
+        const data = res.data
+        setSortState(
+            data.posts.map((post) =>
+                <div key={post.postId} className="border border-slate-700 rounded-xl px-3 py-3">
+                    <Link to={`/cocodogo-client/posts/${post.postId}`} className="flex flex-col text-center">
+                        <p className="py-2">Posted by <strong>{post.username}</strong> on {post.date.toLocaleString()}</p>
+                        <p >upvotes: {data.postVotes.filter((postVote) => postVote.postId === post.postId).reduce((accumulator, currentValue) => accumulator + currentValue.value, 0)}</p>
+                        <h3 className="py-3 text-2xl text-slate-700 font-medium">{post.title}</h3>
+                        <p>{post.content}</p>
+                        <p className="py-3">{data.comments.filter((comment) => comment.postId === post.postId).length + data.replies.filter((reply) => reply.postId === post.postId).length} {data.comments.filter((comment) => comment.postId === post.postId).length + data.replies.filter((reply) => reply.postId === post.postId).length == 1 ? "comment" : "comments"}</p>
+                    </Link>
+                </div>)
+        )
+    }
+
     return (
         <div>
             <h1 className="py-5 text-2xl text-slate-700 font-medium text-center">Posts</h1>
@@ -122,7 +139,7 @@ export default function PostsPage() {
             <div className="md:grid md:gap-4 md:grid-cols-3">
                 {sortState}
             </div>
-            <div className="py-3">Page: {data.pages.map((page) => <button key={page} className="px-3 font-bold">{page}</button>)}</div>
+            <div className="py-3">Page: {data.pages.map((page) => <button key={page} onClick={() => getPageData(page)} className="px-3 font-bold">{page}</button>)}</div>
         </div>
     )
 
